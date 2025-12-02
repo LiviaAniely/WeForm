6d7b292643eb52e1ff7b1ae2c77b00242a9ed97d
livia.almeida <livia.almeida@ccc.ufcg.edu.br>
2025-12-02 18:05:30 -0300

    feat: adiciona tela de listagem de entidades

--- app/entidades/page.tsx
+++ app/entidades/page.tsx
@@ -1,12 +1,65 @@
-import Link from "next/link";
+'use client';
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useRouter } from "next/navigation";
+import './styles.css';
 
 export default function Entidade() { 
+  const router = useRouter();
+
+  function getItemListagem(nomeEntidade:string, campoAtuacao:string) {
+    return (
+      <>
+        <div className="flex items-center justify-between">
+          <span style={{ marginRight: '10px' }}>{nomeEntidade}</span>
+          <h3>{campoAtuacao}</h3>
+        </div>
+        <hr style={{ margin: '10px 0 10px 0' }}/>
+      </>
+    );
+  }
   return (
     <>
-      <h1>Listagem de Entidade</h1>
-      <Link href="/">
-        <button className="button">Voltar à página inicial</button>
-      </Link>
+      <div className="flex flex-col h-full items-center">
+        <h1
+        className="font-bold text-7xl text-center"
+        style={{ marginTop: '50px' }}
+        >
+          <span className="emphasis">We</span> Form
+        </h1>
+        <div
+          className="flex flex-col"
+          style={{
+            border: '1px solid white',
+            width: '40%',
+            height: '80%',
+            marginTop: '50px',
+            padding: '20px',
+            borderRadius: '8px',
+          }}>
+          <div className="flex" style={{ marginBottom: '20px' }}>
+            <button
+            style={{ cursor: 'pointer' }}
+            onClick={() => {
+              router.push("/");
+            }}
+            >
+              <FontAwesomeIcon icon={faAngleLeft} style={{ color: "#ffffff" }} size="2xl" />
+            </button>
+            <h2
+              className="font-bold text-4xl"
+            >
+              <span className="emphasis">Entidades</span> Cadastradas
+            </h2>
+          </div>
+
+          <div id="listagem">
+            {getItemListagem("Abrigo Vida Animal", "Abrigo de Animais")}
+            {getItemListagem("Nosso Prato", "Centro de Alimentação")}
+            {getItemListagem("Creche do Sol", "Creche Infantil")}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
