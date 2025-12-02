da92b03e1622974bbffa4b2206832e7c87e3951d
livia.almeida <livia.almeida@ccc.ufcg.edu.br>
2025-12-01 21:25:13 -0300

    feat: ajusta troca de componentes entre tela inicial e cadastro, adiciona textos e estilos

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,92 @@
+'use client'
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
+  const [showForm, setShowForm] = useState(false);
+  const router = useRouter();
+
+  function cadastrarEntidade() {
+    return (
+      <>
+        <h1>Cadastro de Entidade</h1>
+          <button
+            className="button"
+            onClick={() => {
+              setShowForm(false);
+              router.push("/");
+            }}
+          >
+            Voltar à página inicial
+          </button>
+      </>
+    );
+  }
   return (
     <>
-      <div className="flex flex-row items-center justify-evenly min-h-screen container-home">
+      <div className="flex flex-row items-center justify-center min-h-screen container-home">
         <div className="flex flex-col items-center gap-3">
           <Image
             className="dark:invert"
-            src="/next.svg"
+            src="/main-image.svg"
             alt="Next.js logo"
-            width={100}
-            height={20}
+            width={500}
+            height={500}
           />
-          <h1 className="font-bold"><span className="emphasis">We</span> Form</h1>
+          <h1 className="font-bold text-7xl"><span className="emphasis">We</span> Form</h1>
         </div>
           
-        <div className="flex flex-col items-center gap-3 mt-4">
-          <h2 className="font-bold">Cadastre sua <span className="emphasis">Entidade</span> ou acesse dados de <span className="emphasis">parceiras</span></h2>
-          <Link href="/entidade">
-            <button className="button">Cadastrar Entidades</button>
-          </Link>
-          <button className="button">Ver Entidades Listadas</button>
+        <div
+          style={{
+            padding: '40px',
+            width: '600px',
+            height: '500px',
+            border: '1px solid var(--foreground)',
+            borderRadius: '8px',
+            marginLeft: '40px',
+          }}
+        >
+          {showForm ? (
+            cadastrarEntidade()
+          ) : (
+            <div className="flex flex-col h-full items-center gap-3">
+              <div className="flex flex-col items-center gap-3">
+                <h2
+                  className="font-bold text-4xl text-center"
+                  style={{ marginBottom: '50px' }}
+                >
+                  Cadastre sua <span className="emphasis">Entidade</span> ou acesse dados de <span className="emphasis">parceiras</span>
+                </h2>
+
+                <button
+                  className="button"
+                  onClick={() => setShowForm(true)}
+                >
+                  Cadastrar Entidades
+                </button>
+
+                <button
+                  className="button"
+                  onClick={() => router.push("/entidades")}
+                >
+                  Ver Entidades Listadas
+                </button>
+              </div>
+
+              <div className="mt-auto flex flex-col items-center gap-1">
+                <h3>Sua entidade não se enquadra nos campos de atuação disponíveis?</h3>
+                <button
+                  onClick={() => router.push("/campoAtuacao")}
+                  style={{ textDecoration: 'underline', cursor: 'pointer' }}
+                >
+                  Clique aqui para sugerir um novo campo
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-
       </div>
-
     </>
   );
 }
